780d14724b2d864baae7edd028e649d6b2120fb1
Dev_Ricky <eric.kiogora12@gmail.com>
2022-08-15 12:00:25 +0300

    Done.

---
+++ app.rb
@@ -0,0 +1,11 @@
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
+
+
+
+#puts  prints "Hello world" with line break
+#Prints numbers [1,2,3] with line break
+#Prints numbers [1,2,3] without line break
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
